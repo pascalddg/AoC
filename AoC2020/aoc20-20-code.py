66506596937c433c2a61b89934cdac8193c8e875
pascal.ddg <49622051+pascalddg@users.noreply.github.com>
2021-01-05 20:31:51 +0100

    DFS part1, working but ugly

--- AoC2020/aoc20-20-code.py
+++ AoC2020/aoc20-20-code.py
@@ -1,7 +1,7 @@
 import time
 import math
 from itertools import product
-from collections import deque
+from collections import deque, defaultdict
 
 def load_data():
     with open("AoC2020/aoc20-20-data.txt", "r") as f:
@@ -52,17 +52,17 @@ def get_shapes(p_tile):
         
     return tuple(shapes)
 
-# [id, [lines], [shapes]]
+# [id, [lines], [shapes],[matching tiles]]
 def parse_data(p_data):
     tiles = []
-    tile=[0,[],[]]
+    tile=[0,[],[],[]]
     for line in data:
         if line.startswith("Tile"):
             tile[0]=int(line[5:-1])
         elif not line:
             tile[2]=get_shapes(tile)
             tiles.append(tile)
-            tile=[0,[],[]]
+            tile=[0,[],[],[]]
         else:
             tile[1].append(line)
     if tile[0]:
@@ -71,57 +71,98 @@ def parse_data(p_data):
 
     return tiles
 
-def fit(mosaic, shape, px,py):
-    if not 0<=px<len(mosaic) or not 0<=py<len(mosaic):
+
+def fit(mosaic, shape, px,py, len_mos):
+    if not 0<=px<len_mos or not 0<=py<len_mos:
         return False
 
     if px>0 and mosaic[px-1][py]:
         if mosaic[px-1][py][1][1]!=shape[3]:
             return False
-    if px<len(mosaic)-1 and mosaic[px+1][py]:
+    if px<len_mos-1 and mosaic[px+1][py]:
         if mosaic[px+1][py][1][3]!=shape[1]:
             return False
     if py>0 and mosaic[px][py-1]:
         if mosaic[px][py-1][1][0]!=shape[2]:
             return False
-    if py<len(mosaic)-1 and mosaic[px][py+1]:
+    if py<len_mos-1 and mosaic[px][py+1]:
         if mosaic[px][py+1][1][2]!=shape[0]:
             return False
 
     return True
 
-counter = 0
-maxi = 0
-t1 = time.perf_counter()
-dirc = ((-1,0),(1,0),(0,1),(0,-1))
-def arrange(mosaic, tiles, px, py, stack):
-    global counter
-    global maxi
-    global t1
-    counter +=1
-    maxi = max(maxi,len(stack))
-    if not counter % 10_000:
-        t2 = time.perf_counter()
-        print(counter//1000,' ', maxi, len(stack),' ',px,' ',py, f' TIME = {(t2-t1):.2f}')
-        t1=t2
+#dirc = ((-1,0),(1,0),(0,1),(0,-1))
 
+
+def dfs(mosaic, graf, tile, px, py, stack, depth):
+
+#    print(depth)
+    
     if not 0<=px<len(mosaic) or not 0<=py<len(mosaic) or mosaic[px][py]:
         return False
-    for tile in tiles:
-        if tile not in stack:
-            stack.append(tile)
-            for shape in tile[2]:
-                if fit(mosaic, shape, px,py):
-                    mosaic[px][py] = (tile[0], shape)
-                    if len(stack)==len(tiles):
-                        return True
-                    for d in dirc:
-                        result = arrange(mosaic, tiles, px+d[0], py+d[1], stack)
-                        if result:
-                            return result
-                    mosaic[px][py] = 0
-            stack.pop()
+    len_mos = len(mosaic)
+
+    if tile not in stack:
+        stack.append(tile)
+        for shape in tile[2]:
+            if fit(mosaic, shape, px, py, len_mos):
+                mosaic[px][py] = (tile[0], shape)
+                if len(stack)==len(tiles):
+                    return True
+
+                result = False
+                if px>0 and not mosaic[px-1][py]:
+                    edge_value = shape[3]
+                    matching_tiles = graf.get(edge_value)
+                    for i, item in enumerate(matching_tiles.values()):
+                        if item != tile:
+                            depth.append(1)
+                            result = dfs(mosaic, graf, item, px-1, py, stack, depth)
+                            depth.pop()
+                    if result:
+                        return result
+                    continue
+
+                if px<len(mosaic)-1 and not mosaic[px+1][py] and not result:
+                    edge_value = shape[1]
+                    matching_tiles = graf.get(edge_value)
+                    for i, item in enumerate(matching_tiles.values()):
+                        if item != tile:
+                            depth.append(2)
+                            result = dfs(mosaic, graf, item, px+1, py, stack, depth)
+                            depth.pop()
+                    if result:
+                        return result
+                    continue
+
+                if py>0 and not mosaic[px][py-1] and not result:
+                    edge_value = shape[2]                        
+                    matching_tiles = graf.get(edge_value)
+                    for i, item in enumerate(matching_tiles.values()):
+                        if item != tile:
+                            depth.append(3)
+                            result = dfs(mosaic, graf, item, px, py-1, stack, depth)
+                            depth.pop()
+                    if result:
+                        return result
+                    continue
+
+                if py<len(mosaic)-1 and not mosaic[px][py+1] and not result:
+                    edge_value = shape[0]                        
+                    matching_tiles = graf.get(edge_value)
+                    for i, item in enumerate(matching_tiles.values()):
+                        if item != tile:
+                            depth.append(4)
+                            result = dfs(mosaic, graf, item, px, py+1, stack, depth)
+                            depth.pop()
+                    if result:
+                        return result
+                    continue
+
+        stack.pop()
+    mosaic[px][py] = 0
     return False
+
 
 data, test_data = load_data()
 # uncomment the below line to use test data
@@ -129,29 +170,40 @@ data, test_data = load_data()
 
 #part one
 
-# [id, [lines], edges, edges_flipped]
+# [id, [lines], [shapes],[matching tiles]]
 tiles = parse_data(data)
 edge_len = int(math.sqrt(len(tiles)))
 
-
-
+graf = defaultdict(dict)
+for tile in tiles:
+    for shape in tile[2]:
+        for ival in shape:
+            graf[ival][tile[0]]=tile
 
 mosaic = [[0 for _ in range(edge_len)] for _ in range(edge_len)]
 
-t1 = time.perf_counter()
-arrange(mosaic, tiles, 6, 6, [])
-
-result1 = mosaic[0][0][0]\
-        * mosaic[0][edge_len-1][0]\
-        * mosaic[edge_len-1][0][0]\
-        * mosaic[edge_len-1][edge_len-1][0]
+found = False
+for i,tile in enumerate(tiles):
+    found = dfs(mosaic, graf, tile,0,0,[], [i])
+    if found:
+        break
+if found:
+    result1 = mosaic[0][0][0]\
+            * mosaic[0][edge_len-1][0]\
+            * mosaic[edge_len-1][0][0]\
+            * mosaic[edge_len-1][edge_len-1][0]
+else:
+    result1 = 'Not found.'
 print(f'result 1 = {result1}')
-
+if result1==13224049461431:
+    print(' -=OK=- ')
+else:
+    print(' --err-- ')
 #part two 
 t1 = time.perf_counter()
 
 result2 = None
 
-print(f'result 2 = {result2}')
+#print(f'result 2 = {result2}')
 t2 = time.perf_counter()
-print(f'TIME = {(t2-t1):.2f}') 
+#print(f'TIME = {(t2-t1):.2f}') 
